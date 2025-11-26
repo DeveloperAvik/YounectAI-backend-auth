43e186b25e8a6bd4f4221712df163cd25c3a6746
developeravik <avikd906@proton.me>
2025-11-26 08:20:59 +0530

    setup prisma and build initial srtucture

---
+++ auth/src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Auth Service Running...");
+});
+
+export default app;
